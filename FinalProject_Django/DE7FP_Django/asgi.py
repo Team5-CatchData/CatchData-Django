988d57b0f75debc6543a8841c8ab6ce864a1c274
JeongJaeHyeon <zhfem05@gmail.com>
2025-12-12 11:51:57 +0900

    chore: DE7FP_Django로 설정 바꿈

--- FinalProject_Django/DE7FP_Django/asgi.py
+++ FinalProject_Django/DE7FP_Django/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DE7_Final_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DE7FP_Django.settings')
 
 application = get_asgi_application()
