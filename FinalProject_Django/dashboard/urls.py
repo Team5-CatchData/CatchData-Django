eb3f344df77879b5c0e24b295a76b7adeab6226d
JeongJaeHyeon <zhfem05@gmail.com>
2025-12-16 11:56:24 +0900

    feature: 인기 순위 대시보드, 워드클라우드 추가

--- FinalProject_Django/dashboard/urls.py
+++ FinalProject_Django/dashboard/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('api/filter-options/', views.get_filter_options, name='filter_options'),
     path('api/filter-restaurants/', views.filter_restaurants, name='filter_restaurants'),
+    path('api/top-restaurants/', views.get_top_restaurants, name='top_restaurants'),
+    path('api/top-categories/', views.get_top_categories, name='top_categories'),
 ]
--- FinalProject_Django/main/models.py
+++ FinalProject_Django/main/models.py
@@ -14,6 +14,7 @@ class Restaurant(models.Model):
     y = models.FloatField(null=True, blank=True, verbose_name="위도")
     region = models.CharField(max_length=20, blank=True, verbose_name="지역")
     city = models.CharField(max_length=15, blank=True, verbose_name="도시")
+    waitting = models.IntegerField(null=True, blank=True, verbose_name="대기 인원")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성일시")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="수정일시")
 
