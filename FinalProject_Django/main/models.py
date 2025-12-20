1863f909548e35863bd27a24522d1f532e3bd817
JeongJaeHyeon <zhfem05@gmail.com>
2025-12-20 10:08:35 +0900

    fix: models 수정

--- FinalProject_Django/main/models.py
+++ FinalProject_Django/main/models.py
@@ -15,8 +15,9 @@ class Restaurant(models.Model):
     region = models.CharField(max_length=20, blank=True, verbose_name="지역")
     city = models.CharField(max_length=15, blank=True, verbose_name="도시")
     waitting = models.IntegerField(null=True, blank=True, verbose_name="대기 인원")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성일시")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="수정일시")
+    rec_quality = models.FloatField(null=True, blank=True, verbose_name="추천 품질 점수")
+    rec_balanced = models.FloatField(null=True, blank=True, verbose_name="추천 균형 점수")
+    rec_convenience = models.FloatField(null=True, blank=True, verbose_name="추천 편의 점수")
 
     class Meta:
         verbose_name = "레스토랑"
