997341c0082a1a8833398b595ffcfab2266e557d
JeongJaeHyeon <zhfem05@gmail.com>
2025-12-22 11:40:41 +0900

    feature: 추천도 대시보드 구현

--- FinalProject_Django/dashboard/urls.py
+++ FinalProject_Django/dashboard/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('api/top-restaurants/', views.get_top_restaurants, name='get_top_restaurants'),
     path('api/top-categories/', views.get_top_categories, name='get_top_categories'),
+    path('api/top-by-recommendation/', views.get_top_by_recommendation, name='get_top_by_recommendation'),
     path('api/filter-options/', views.get_filter_options, name='get_filter_options'),
     path(
         'api/filter-restaurants/', views.filter_restaurants, name='filter_restaurants'
